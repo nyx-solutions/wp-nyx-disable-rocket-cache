24dd72dcd28be39b78d7422add6ca1458034da97
Jonatas Sas <atendimento@jsas.com.br>
2020-05-22 14:08:47 -0300

    Plugin

---
+++ wp-nox-disable-rocket-cache.php
@@ -0,0 +1,41 @@
+<?php
+
+	/**
+	 * Plugin Name: NOX | Disable WP Rocket Cache
+	 * Description: Disables WP Rocket’s page cache while preserving other optimization features. In order to enable the funcionality, use the constant NOX_DISABLE_ROCKET_CACHE with true
+	 * Plugin URI:  https://github.com/nox-it/wp-nox-disable-rocket-cache
+	 * Author:      Jonatas Sas
+	 * Author URI:  https://www.jsas.com.br/
+	 * License:     GNU General Public License v2 or later
+	 * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+	 */
+
+	defined( 'ABSPATH' ) or die();
+
+	defined( 'NOX_DISABLE_ROCKET_CACHE' ) or define( 'NOX_DISABLE_ROCKET_CACHE', false );
+
+	if (NOX_DISABLE_ROCKET_CACHE) {
+		/**
+		 * Disable page caching in WP Rocket.
+		 *
+		 * @link http://docs.wp-rocket.me/article/61-disable-page-caching
+		 */
+		add_filter( 'do_rocket_generate_caching_files', '__return_false' );
+
+		/**
+		 * Cleans entire cache folder on activation.
+		 */
+		function nox_clean_wp_rocket_cache() {
+
+			if ( ! function_exists( 'rocket_clean_domain' ) ) {
+				return false;
+			}
+
+			// Purge entire WP Rocket cache.
+			rocket_clean_domain();
+
+			return true;
+		}
+
+		register_activation_hook( __FILE__, __NAMESPACE__ . 'nox_clean_wp_rocket_cache' );
+	}
